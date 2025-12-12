fbc9e6f33456a846ab7bc870a3c8c5d297170c04
vaibhaviimcal-web <vaibhav.iimcal@gmail.com>
2025-12-13 04:06:35 +0530

    Update App with telemedicine routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,15 @@ import ReceptionistDashboard from './pages/dashboards/ReceptionistDashboard'
 import LabTechDashboard from './pages/dashboards/LabTechDashboard'
 import PharmacistDashboard from './pages/dashboards/PharmacistDashboard'
 import PatientDashboard from './pages/dashboards/PatientDashboard'
+import VideoCall from './pages/telemedicine/VideoCall'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RoleSelection />} />
+        
+        {/* Dashboard Routes */}
         <Route path="/dashboard/super-admin" element={<SuperAdminDashboard />} />
         <Route path="/dashboard/admin" element={<AdminDashboard />} />
         <Route path="/dashboard/doctor" element={<DoctorDashboard />} />
@@ -22,6 +25,11 @@ function App() {
         <Route path="/dashboard/lab-tech" element={<LabTechDashboard />} />
         <Route path="/dashboard/pharmacist" element={<PharmacistDashboard />} />
         <Route path="/dashboard/patient" element={<PatientDashboard />} />
+        
+        {/* Telemedicine Routes */}
+        <Route path="/dashboard/doctor/telemedicine" element={<VideoCall />} />
+        <Route path="/dashboard/patient/telemedicine" element={<VideoCall />} />
+        
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
